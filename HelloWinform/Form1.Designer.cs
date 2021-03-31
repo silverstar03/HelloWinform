5185a01e4c2cf453198cc9c3a13e7b1e68dfeae6
silverstar03 <eunbyoul0421@naver.com>
2021-03-31 10:13:15 +0900

    버튼 추가 및 버튼 클릭 핸들러 코드 작성

--- HelloWinform/Form1.Designer.cs
+++ HelloWinform/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace HelloWinform
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 196);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "클릭하세요!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(310, 221);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "나도 버튼";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(349, 478);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
